f973b1faf77b573589322c1e47c97167402a4684
ElshadHamidov <ehamidov09@gmail.com>
2024-09-12 17:27:29 +0400

    lesson3

---
+++ codes/javase/JavaSeCode/JavaSeCode/src/lesson3/lesson3.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package lesson3;
+
+/**
+ * 
+ */
+public class lesson3 {
+
+	/**
+	 * 
+	 */
+	public lesson3() {
+		// TODO Auto-generated constructor stub
+	}
+	public static void main(String[] args) {
+		int age = 7;
+		age = 14;
+		byte b = 7;
+		short s = 7;
+		long l = 7;
+		double d = 7.9D;
+		float f = 7.9F;
+		char c = 'A';
+		boolean hasHome = true ;
+		System.out.println(age);
+		System.out.println(b);
+		System.out.println(s);
+		System.out.println(l);
+		System.out.println(d);
+		System.out.println(f);
+		System.out.println(c);
+		System.out.println(hasHome);
+
+	}
+
+}
