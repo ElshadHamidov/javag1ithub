5a6dfd08b735b47b89d9491b1daa4a465a353e2a
ElshadHamidov <ehamidov09@gmail.com>
2024-12-14 08:12:06 +0400

    OOPFINAL

---
+++ codes/javase/JavaSeCode/JavaSeCode/src7/oop1/Animal.java
@@ -0,0 +1,7 @@
+package oop1;
+
+abstract class Animal {
+    String name;
+    int age;
+    public abstract void makeNoise();  
+}
---
+++ codes/javase/JavaSeCode/JavaSeCode/src7/oop1/Cat.java
@@ -0,0 +1,12 @@
+package oop1;
+
+public class Cat extends Animal {
+    public Cat(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    @Override
+    public void makeNoise() {
+        System.out.println(name + ": Noise!");
+    }
+}
