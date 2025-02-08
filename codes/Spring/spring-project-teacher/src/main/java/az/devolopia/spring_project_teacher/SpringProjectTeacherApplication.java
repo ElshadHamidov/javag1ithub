3ef783b94f8b6184fa0258ce8414f21a1c95bc4b
ElshadHamidov <ehamidov09@gmail.com>
2025-02-08 16:44:32 +0400

    Spring

---
+++ codes/Spring/spring-project-teacher/src/main/java/az/devolopia/spring_project_teacher/SpringProjectTeacherApplication.java
@@ -0,0 +1,13 @@
+package az.devolopia.spring_project_teacher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProjectTeacherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProjectTeacherApplication.class, args);
+	}
+
+}
---
+++ codes/Spring/spring-project-teacher/src/test/java/az/devolopia/spring_project_teacher/SpringProjectTeacherApplicationTests.java
@@ -0,0 +1,13 @@
+package az.devolopia.spring_project_teacher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProjectTeacherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
