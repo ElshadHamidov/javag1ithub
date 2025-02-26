c0cc18400f908ed5e6d6a5bc262cf6204ac32e3a
ElshadHamidov <ehamidov09@gmail.com>
2025-02-26 21:50:56 +0400

    s

--- codes/SpringWeb/stringweb/src/main/java/az/developis/stringweb/controller/BookController.java
+++ codes/SpringWeb/stringweb/src/main/java/az/developis/stringweb/controller/BookController.java
@@ -1,12 +1,23 @@
 package az.developis.stringweb.controller;
 
+import az.developis.stringweb.entity.Book;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
 @Controller
 public class BookController {
 
     @GetMapping("/books")
-    public String getBooks() {
+    public String getBooks(Model model) {
+        List<Book> books = List.of(
+            new Book("The Alchemist", "Paulo Coelho", 1988),
+            new Book("1984", "George Orwell", 1949),
+            new Book("To Kill a Mockingbird", "Harper Lee", 1960)
+        );
+        model.addAttribute("books", books);
         return "books";
     }
 }
