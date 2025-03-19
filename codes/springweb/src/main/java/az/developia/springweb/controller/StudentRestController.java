9409e806d85e76c755746b7a5ac3ab5ebd9942a5
ElshadHamidov <ehamidov09@gmail.com>
2025-03-19 08:35:41 +0400

    Ancaq Update çatdırdım amma işləyir kod

--- codes/springweb/src/main/java/az/developia/springweb/controller/StudentRestController.java
+++ codes/springweb/src/main/java/az/developia/springweb/controller/StudentRestController.java
@@ -23,4 +23,22 @@ public class StudentRestController {
         System.out.println(student);
         StudentRepository.save(student);
     }
+
+    // null, 0, found, not found
+    @PutMapping(path = "/update")
+    public void update(@Valid @RequestBody Student student, BindingResult br) {
+        if (br.hasErrors()) {
+            throw new OurRuntimeException(null, "melumatlarin tamliginda problem var");
+        }
+
+        if (student.getId() == null || student.getId() == 0) {
+            throw new OurRuntimeException(null, "id mutleqdir");
+        }
+
+        if (studentRepository.findById(student.getId()).isPresent()) {
+            studentRepository.save(student);
+        } else {
+            throw new OurRuntimeException(null, "id tapilmadi");
+        }
+    }
 }
