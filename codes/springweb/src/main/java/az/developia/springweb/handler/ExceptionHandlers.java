02a2e22f591a40eec0f3e2cee4df6ce35023d2af
ElshadHamidov <ehamidov09@gmail.com>
2025-03-29 15:04:05 +0400

    .

--- codes/springweb/src/main/java/az/developia/springweb/handler/ExceptionHandlers.java
+++ codes/springweb/src/main/java/az/developia/springweb/handler/ExceptionHandlers.java
@@ -6,8 +6,28 @@ import az.developia.springweb.exception.OurRuntimeException;
 
 @RestControllerAdvice
 public class ExceptionHandlers {
-    @ExceptionHandler(OurRuntimeException.class)
-    public String handle(OurRuntimeException exc) {
-        return exc.getBr().getFieldErrors().get(0).getField();
+
+    @ExceptionHandler
+    @ResponseStatus(code = HttpStatus.BAD_REQUEST)
+    public ExceptionResponse handle(OurRuntimeException exc) {
+
+        ExceptionResponse response = new ExceptionResponse();
+
+        BindingResult br = exc.getBr();
+        if (br == null) {
+
+        } else {
+            List<FieldError> fieldErrors = br.getFieldErrors();
+            List<ValidationResponse> validations = new ArrayList<ValidationResponse>();
+            for (FieldError error : fieldErrors) {
+                ValidationResponse validation = new ValidationResponse();
+                validation.setField(error.getField());
+                validation.setErrorMessage(error.getDefaultMessage());
+                validations.add(validation);
+            }
+            response.setValidations(validations);
+        }
+        response.setMessage(exc.getMessage());
+        return response;
     }
 }
