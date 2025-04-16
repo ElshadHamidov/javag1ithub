9330265c3e15e686b62046e0d89d160b87e4519b
ElshadHamidov <ehamidov09@gmail.com>
2025-04-16 14:04:33 +0400

    Username

--- codes/manga/src/main/java/az/book/manga/model/Reader.java
+++ codes/manga/src/main/java/az/book/manga/model/Reader.java
@@ -1,19 +1,31 @@
 package az.book.manga.model;
 
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@Table(name = "readers")
 @Getter
 @Setter
-@NoArgsConstructor
 @AllArgsConstructor
+@NoArgsConstructor
 public class Reader {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     private String name;
-    private int age;
+    private Integer age;
     private String email;
+
+    private String username;
+    private String password;
 }
