b8b6641c2526efccbcd62e12c55d604ba6d8efbf
ElshadHamidov <ehamidov09@gmail.com>
2025-06-14 17:40:30 +0400

    9

--- codes/JavaSeCode/srctest/dovr/JavaTest.java
+++ codes/JavaSeCode/srctest/dovr/JavaTest.java
@@ -1,14 +1,24 @@
 package dovr;
 
+import java.util.Scanner;
+
 public class JavaTest {
     public static void main(String[] args) {
         // int sum=0;
-        for(int i=1; i<=100; i++){
-            // sum+=i;
-            if(i%7==0){
-                System.out.println(i);
-            }
-        }
+        // for(int i=1; i<=100; i++){
+        // sum+=i;
+        // if(i%7==0){
+        // System.out.println(i);
+        // }
+        // }
         // System.out.println(sum);
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        scanner.close();
+        if (number % 2 == 0) {
+            System.out.println("Cüt ədəddir");
+        } else {
+            System.out.println("Tək ədəddir");
+        }
     }
 }
