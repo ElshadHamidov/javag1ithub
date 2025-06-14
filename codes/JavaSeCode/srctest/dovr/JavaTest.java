41de8e4dd52e541247dc96b0bdf0e80ac8ad601a
ElshadHamidov <ehamidov09@gmail.com>
2025-06-14 17:32:47 +0400

    8

--- codes/JavaSeCode/srctest/dovr/JavaTest.java
+++ codes/JavaSeCode/srctest/dovr/JavaTest.java
@@ -2,10 +2,13 @@ package dovr;
 
 public class JavaTest {
     public static void main(String[] args) {
-        int sum=0;
+        // int sum=0;
         for(int i=1; i<=100; i++){
-            sum+=i;
+            // sum+=i;
+            if(i%7==0){
+                System.out.println(i);
+            }
         }
-        System.out.println(sum);
+        // System.out.println(sum);
     }
 }
