23d5e2c2eaf7799d114bb155a29f5750d9e5d055
ElshadHamidov <ehamidov09@gmail.com>
2025-06-15 20:17:43 +0400

    Ex:1

--- codes/JavaSeCode/srctest/dovr/JavaTest.java
+++ codes/JavaSeCode/srctest/dovr/JavaTest.java
@@ -1,24 +1,28 @@
 package dovr;
 
-// import java.util.Scanner;
+import java.util.Scanner;
 
 public class JavaTest {
     public static void main(String[] args) {
         // int sum=0;
-        float i=0;
-        while (i<=5) {
-            i+=0.1;
-            System.out.println(i);
-        }
+        // float i=0;
+        // while (i<=5) {
+        //     i+=0.1;
+        //     System.out.println(i);
+        // }
         // }
         // System.out.println(sum);
-        // Scanner scanner = new Scanner(System.in);
-        // int number = scanner.nextInt();
-        // scanner.close();
-        // if (number % 2 == 0) {
-        // System.out.println("Cüt ədəddir");
-        // } else {
-        // System.out.println("Tək ədəddir");
-        // }
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int number = scanner.nextInt();
+        int sum = 0;
+
+        while (number != 0) {
+            sum += number % 10;
+            number /= 10;
+        }
+
+        System.out.println("Sum of Number: " + sum);
+        scanner.close();
     }
 }
