80a62bba1b71c54973b6c2847086044ea8659560
ElshadHamidov <ehamidov09@gmail.com>
2025-07-18 17:22:36 +0400

    Done

--- codes/New folder/manga/src/main/java/az/book/manga/controller/FileController.java
+++ codes/New folder/manga/src/main/java/az/book/manga/controller/FileController.java
@@ -3,16 +3,25 @@ package az.book.manga.controller;
 
 import java.io.File;
 import java.io.InputStream;
+import java.net.MalformedURLException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.util.UUID;
 
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -51,5 +60,32 @@ public class FileController {
 		Files.copy(stream, Paths.get("C:/Downloads/TestSubject" + "/" + randomFileName), StandardCopyOption.REPLACE_EXISTING);
 		
 		
+	}
+
+	@GetMapping("/download/{filename:.+}")
+	@ResponseBody
+	public ResponseEntity<Resource> seveFile(@PathVariable String filename) throws Exception {
+
+		Resource file = loadAsResource(filename);
+		return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + filename + "\"")
+				.body(file);
+	}
+
+	public Resource loadAsResource(String filename) {
+		try {
+			Path file = load(filename);
+			Resource resource = new UrlResource(file.toUri());
+			if (resource.exists() || resource.isReadable()) {
+				return resource;
+			}
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public Path load(String filename) {
+		Path rootLocation = Paths.get()"C:/Downloads/TestSubject");
+		return rootLocation.resolve(filename);
 	}
 }
